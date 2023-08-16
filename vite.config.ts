9edc5943a00637b558b9df825040713b0a67089a
shaman-development <shaman.development@gmail.com>
2023-08-16 20:50:50 +0200

    Basic setup

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,46 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     VitePWA({
-      registerType: 'autoUpdate',
+      strategies: 'injectManifest',
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      },
+      srcDir: 'src',
+      filename: 'sw.js',
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'MusicMind',
+        short_name: 'MusicMind',
+        description: 'MusicMind - share what plays in your mind',
+        theme_color: '#0af2f2',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
       }
     }),
     vue()
