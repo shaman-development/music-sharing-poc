109276a94b42cc4df244e7f1c99606fcdf850dd4
shaman-development <shaman.development@gmail.com>
2023-08-20 18:36:32 +0200

    Support normal query params in create-post

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -6,36 +6,39 @@ import { useUser } from '@/stores/user'
 import supabase from '@/plugins/supabase'
 import NotificationBanner from '@/components/NotificationBanner.vue'
 import LoadingAnimation from '@/components/LoadingAnimation.vue'
-import {useRoute} from "vue-router";
+import { useRoute } from 'vue-router'
 
-const route = useRoute();
+const route = useRoute()
 
-const sharedUrl = (typeof route.query?.url === 'string' && route.query?.url) || ''
-const url = ref(sharedUrl);
+const sharedUrl =
+  (typeof route.query?.url === 'string' && route.query?.url) ||
+  (window.location.search && new URLSearchParams(window.location.search).get('url')) ||
+  ''
+const url = ref(sharedUrl)
 
 const errorMessage = ref()
-const isSuccess = ref(false);
-const isSubmittingPost = ref(false);
+const isSuccess = ref(false)
+const isSubmittingPost = ref(false)
 const handleSubmit = async () => {
   if (!hasSongId.value) return
-  isSubmittingPost.value = true;
+  isSubmittingPost.value = true
   errorMessage.value = null
-  isSuccess.value = false;
+  isSuccess.value = false
 
   const { error } = await supabase.from('posts').insert({ content: url.value })
   if (error) {
     errorMessage.value = error?.message
-    isSuccess.value = false;
+    isSuccess.value = false
   }
 
-  isSuccess.value = true;
-  isSubmittingPost.value = false;
-  url.value = '';
+  isSuccess.value = true
+  isSubmittingPost.value = false
+  url.value = ''
 }
 
 const hasSongId = computed(() => !!getSongIdFromUrl(url.value))
 
-const now = new Date();
+const now = new Date()
 
 const { username } = useUser()
 </script>
